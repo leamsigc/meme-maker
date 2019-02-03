85834fbd4f6f12f365f039e411071b6b1ca70eaf
leamsigc <leamsigc@hotmail.com>
2019-02-03 00:46:56 -0500

    Updates

---
+++ precache-manifest.add92bd24078c7b5095d19527cfe142a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "873211a92c1bf7316591",
+    "url": "/meme-maker/static/css/main.8b4ab7c1.chunk.css"
+  },
+  {
+    "revision": "873211a92c1bf7316591",
+    "url": "/meme-maker/static/js/main.873211a9.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/meme-maker/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "c788a6a292c912a6df74",
+    "url": "/meme-maker/static/js/runtime~main.c788a6a2.js"
+  },
+  {
+    "revision": "c1ea9712a52c45b3a96f79ef9442e2a6",
+    "url": "/meme-maker/index.html"
+  }
+];
